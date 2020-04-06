4604c52e56ad75c267f1d515fae68668ba655621
AnastasiyaFeofanova <feoanastasiya@gmail.com>
2020-04-06 13:03:24 +0300

    add few tests for basket and basket PO

--- pages/locators.py
+++ pages/locators.py
@@ -12,7 +12,12 @@ class AddToBasketLocators():
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    BASKET_LINK = (By.CSS_SELECTOR, "span.btn-group a.btn-default")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class BasketPageLocaors():
+    BASKET_IS_EMPTY = (By.CSS_SELECTOR, "#content_inner p")
+    CONTENT = (By.CSS_SELECTOR, "h2.col-sm-6")
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,13 @@
 import pytest
 from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
+from pages.locators import BasketPageLocaors
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear" #link for first task
 link1 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019" #link for second task
 link2 = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-'''@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -41,7 +43,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_to_basket()
     page.should_dissapear_message()
-'''
+
 def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link2)
     page.open()
@@ -53,3 +55,11 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.guest_should_not_seen_products()
+    basket_page.should_be_empty()
